e3974c029dccf73d9aecf6e3fab7e09c08ce5964
Ishwarya Sundhararajan <80136488+iSundhararajan@users.noreply.github.com>
2023-02-04 13:50:16 -0500

    Create form.js

---
+++ form.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import bgimg from "../assets/bgimg.webp";
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
+function form() {
+  <Stack direction='row' spacing={4} align='center'>
+  <Button colorScheme='purple' variant='solid'>
+    Join
+  </Button>
+  <Button colorScheme='teal' variant='solid'>
+    Sponser
+  </Button>
+  <Button colorScheme='teal' variant='outline'>
+    Share
+  </Button>
+
+</Stack>
+}
+
+export default form;
